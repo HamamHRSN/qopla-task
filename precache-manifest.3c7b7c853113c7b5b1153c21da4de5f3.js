977ca9a48964b2a6feedb914443c074f89843f44
HamamHRSN <hamamhamou.hrsa@gmail.com>
2019-08-28 00:21:42 +0200

    Updates

---
+++ precache-manifest.3c7b7c853113c7b5b1153c21da4de5f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263f2ee42508b77be8be67c4289ecf94",
+    "url": "/qopla-task/index.html"
+  },
+  {
+    "revision": "966466fa56503cf4e5d5",
+    "url": "/qopla-task/static/css/main.343d77c5.chunk.css"
+  },
+  {
+    "revision": "271553b708fa677ab1e1",
+    "url": "/qopla-task/static/js/2.dbf0b65c.chunk.js"
+  },
+  {
+    "revision": "966466fa56503cf4e5d5",
+    "url": "/qopla-task/static/js/main.6e0306fb.chunk.js"
+  },
+  {
+    "revision": "ca416c92cf6301d1710e",
+    "url": "/qopla-task/static/js/runtime~main.f91fff3a.js"
+  }
+]);
